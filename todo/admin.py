5ac731dc213b894551fe76d318e50bb0c33c3bc1
shohjahon <shohjahonobruyev3@gmail.com>
2024-08-02 14:14:24 +0500

    icon

--- todo/admin.py
+++ todo/admin.py
@@ -2,17 +2,24 @@ from django.contrib import admin
 from todo.models import *
 @admin.register(Task)
 class VersionsAdmin(admin.ModelAdmin):
-    pass
+    list_display = ( 'title', 'priority', 'completed' )
 
 
 @admin.register(Event)
 class VersionsAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('event_name',"startDate")
+    search_fields = ('event_name',)
 
 @admin.register(Expense)
 class VersionsAdmin(admin.ModelAdmin):
-    pass
-
-@admin.register(Icon)
+    list_display = ('name',"title",'price')
+    search_fields = ('name','title','price')
+@admin.register(IconColor)
 class VersionsAdmin(admin.ModelAdmin):
     pass
+
+class IconAdmin(admin.ModelAdmin):
+    list_display = ('name', 'uuid', 'created_at', 'updated_at')
+    search_fields = ('name',)
+
+admin.site.register(Icon, IconAdmin)
--- todo/models.py
+++ todo/models.py
@@ -1,50 +1,62 @@
 import datetime
 import uuid
 from django.db import models
-from django.db.models import ForeignKey
 from django.utils import timezone
 from account.models import CustomUser
 
 class Task(models.Model):
     uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=255)
-    icon = ForeignKey('Icon', related_name='task_icons', on_delete=models.CASCADE)
-    priority = models.CharField(max_length=255, default=0)
-    notes = models.CharField(max_length=255)
-    start_date = models.DateTimeField(default=datetime.datetime.now)
-    due_date = models.DateTimeField(default=timezone.now() + timezone.timedelta(days=1))
+    icon = models.ForeignKey('Icon', related_name='task_icons', on_delete=models.CASCADE)
+    iconColor = models.ForeignKey('IconColor', related_name='task_icons_color', on_delete=models.CASCADE, default=1)
+    priority = models.CharField(max_length=255, default='0')
+    notes = models.CharField(max_length=255, null=True, blank=True)
+    start_date = models.DateTimeField(default=timezone.now)
+    due_date = models.DateTimeField(default= timezone.now() + timezone.timedelta(days=1), null=True)
     completed = models.BooleanField(default=False)
-    geoPoint = models.CharField(max_length=255)
-    created_at = models.DateField(auto_now_add=True)
-    updated_at = models.DateField(auto_now=True)
+    geoPoint = models.CharField(max_length=255, null=True, blank=True)
+    created_at = models.DateField(auto_now_add=True, null=True)
+    updated_at = models.DateField(auto_now=True,null=True)
 
 class Expense(models.Model):
     uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
     title = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
-    icon = ForeignKey("Icon", related_name='expense_icons', on_delete=models.CASCADE)
-    price = models.DecimalField(max_digits=5, decimal_places=2, default=0)
+    icon = models.ForeignKey("Icon", related_name='expense_icons', on_delete=models.CASCADE)
+    iconColor = models.ForeignKey('IconColor', related_name='expense_icons_color', on_delete=models.CASCADE, default=1)
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     is_income = models.BooleanField(default=False)
+    created_at = models.DateField(auto_now_add=True,null=True)
+    updated_at = models.DateField(auto_now=True,null=True)
 
 class Event(models.Model):
     uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     event_name = models.CharField(max_length=255)
     repeatTime = models.CharField(max_length=255)
-    icon = ForeignKey("Icon", related_name='event_icons', on_delete=models.CASCADE,null=True)
-    addNote = models.CharField(max_length=255, null=True)
-    startDate = models.DateField(default=datetime.date.today)
-    endDate = models.DateField(default=timezone.now() + timezone.timedelta(days=1))
-    startTime = models.TimeField(default=datetime.time())
-    endTime = models.TimeField(default=datetime.time())
+    icon = models.ForeignKey("Icon", related_name='event_icons', on_delete=models.CASCADE, null=True)
+    iconColor = models.ForeignKey('IconColor', related_name='event_icons_color', on_delete=models.CASCADE, null=True)
+    addNote = models.CharField(max_length=255, null=True, blank=True)
+    startDate = models.DateField(default=timezone.now)
+    endDate = models.DateField(default= timezone.now() + timezone.timedelta(days=1))
+    startTime = models.TimeField(default=timezone.now)
+    endTime = models.TimeField(default=timezone.now)
     is_all_day = models.BooleanField(default=False)
-    created_at = models.DateField(auto_now_add=True)
-    updated_at = models.DateField(auto_now=True)
-
+    created_at = models.DateField(auto_now_add=True,null=True)
+    updated_at = models.DateField(auto_now=True,null=True)
 
 class Icon(models.Model):
     uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
     image = models.FileField(upload_to='icons/')
-    created_at = models.DateField(auto_now_add=True)
-    updated_at = models.DateField(auto_now=True)
+    created_at = models.DateField(auto_now_add=True,null=True)
+    updated_at = models.DateField(auto_now=True,null=True)
+    def __str__(self):
+        return self.name
+class IconColor(models.Model):
+    color = models.CharField(max_length=255)
+    created_at = models.DateField(auto_now_add=True,null=True)
+    updated_at = models.DateField(auto_now=True,null=True)
+
+    def __str__(self):
+        return self.color
